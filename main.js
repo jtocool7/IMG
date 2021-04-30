8a2656f2699d3c11679d7a25eb77f22a9918bad0
jtocool7 <66321333+jtocool7@users.noreply.github.com>
2021-04-30 18:58:40 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,124 @@
+var codeMyster = new Image();
+codeMyster.src = "https://yt3.ggpht.com/ytc/AAUvwnjc1qIJ5fkEjwjfSMriNGMrW0AFTtuOzXd9xOGs=s176-c-k-c0x00ffffff-no-rj";
+
+var mrBeast = new Image();
+mrBeast.src = "https://yt3.ggpht.com/ytc/AAUvwniEFOinRxSiy4P8PMVW5i0gZ2x0eXeyZ6HQfpnpZw=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var mrBeastReacts = new Image();
+mrBeastReacts.src = "https://yt3.ggpht.com/ytc/AAUvwngaw-bNwl3Wx6SilEMFE-bOiZdkCwrCWjdEz-2UPA=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var mrBeastGaming = new Image();
+mrBeastGaming.src = "https://yt3.ggpht.com/ytc/AAUvwngI68VPilgrZfI5XpT4GZzjSBinXxx3sW6MxPgyrQ=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var mrBro = new Image();
+mrBro.src = "https://yt3.ggpht.com/ytc/AAUvwng4xCkZXKQADCahJt2sKU-urQ1OAJCjSVRmKuzWcw=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var mrBeastPhil = new Image();
+mrBeastPhil.src = "https://yt3.ggpht.com/ytc/AAUvwnhXZiWa-_hO63k9TwhwusYLDImrKDsb2CjYNqpB=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var hackSmith = new Image();
+hackSmith.src = "https://yt3.ggpht.com/ytc/AAUvwni4Pd7D8nyy-OlF-6jy1R4nyuFV7NymZMjEp0dBVg=s68-c-k-c0x00ffffff-no-rj";
+
+var allenPan = new Image();
+allenPan.src = "https://yt3.ggpht.com/ytc/AAUvwnjlfv67qxFKtPDOrSaVp1ciqiAFtMoGzQs-NG036Q=s68-c-k-c0x00ffffff-no-rj";
+
+var JRE = new Image();
+JRE.src = "https://yt3.ggpht.com/ytc/AAUvwni7jM17G7DNPF1g5kIwP75_xRX4OGL7mpPygPxZPA=s68-c-k-c0x00ffffff-no-rj";
+
+var LEX = new Image();
+LEX.src = "https://yt3.ggpht.com/ytc/AAUvwnhhSmkH8EQ0RFKkS1nVkFSBQrggWnqxo1IAqZdDJw=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var Kairos = new Image();
+Kairos.src = "https://yt3.ggpht.com/ytc/AAUvwnh8dpXyVqJB-ARoe-G8GYurUfC3chHwrcyj_HTr-w=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var Rey = new Image();
+Rey.src = "https://yt3.ggpht.com/ytc/AAUvwnjXjEs1xyQhgQySNujRp6GvR7x7tG8AohNOEjhEvA=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var BT1 = new Image();
+BT1.src = "https://yt3.ggpht.com/ytc/AAUvwniBgOewrIZt7OjeYzXtJTH2c8PD_59mWV3ReiXy_A=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var coachCory = new Image();
+coachCory.src = 'https://yt3.ggpht.com/ytc/AAUvwnhjh1qUVPiX_TJc6OFy4kd9ZbOb3inwAbcA6CtU4Q=s176-c-k-c0x00ffffff-no-rj-mo';
+
+var cheifPat = new Image();
+cheifPat.src = "https://yt3.ggpht.com/ytc/AAUvwngGlGiD_134x5QdctlYke7TMh2SeLdF1vcaFIXV1Q=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var NAT = new Image();
+NAT.src = "https://yt3.ggpht.com/ytc/AAUvwniqlsOOCo6-cpZlLzREQ3qwRYMhHl7HdsvfrjSYuQ=s68-c-k-c0x00ffffff-no-rj";
+
+var Tribe = new Image();
+Tribe.src = "https://yt3.ggpht.com/ytc/AAUvwnj03rSVJlrtrWxDgc_CMjPkcQm6pdeBBGbstEdNpg=s68-c-k-c0x00ffffff-no-rj";
+
+var OJ = new Image();
+OJ.src = "https://yt3.ggpht.com/ytc/AAUvwniJv_EUm2wqPlGyPmFubtEfurlaphap8ix66N4gfg=s68-c-k-c0x00ffffff-no-rj";
+var MR = new Image();
+MR.src = "https://yt3.ggpht.com/ytc/AAUvwnjkCEa45O-45oyqizcbgxfZwbL6OeZ_CIZCPQsE6Q=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var MKBHD = new Image();
+MKBHD.src = "https://yt3.ggpht.com/ytc/AAUvwngW9TQgw7E7NqS3Qzd3Up3tjUzkpvMXPWAhYf3LaQ=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var XEETHCARE = new Image();
+XEETHCARE.src = "https://yt3.ggpht.com/ytc/AAUvwnjlivi7WpFHm1wgiUD0jHdsGaXC_gD6palku7hicA=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var TTTV = new Image();
+TTTV.src = "https://yt3.ggpht.com/ytc/AAUvwngeBxdftqKM18DNDl45abGIKFS8PiNlZ04_y2qUMQ=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var techSpurt = new Image();
+techSpurt.src = "https://yt3.ggpht.com/ytc/AAUvwnglhzm8LrNM58elCTlQA-W1vozROZ640RM7cjA=s68-c-k-c0x00ffffff-no-rj";
+
+var TD = new Image();
+TD.src = "https://yt3.ggpht.com/ytc/AAUvwniekiNhGA42nZ9P16WgQO-SX78qxu4wm9SF8_0ahg=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var N3 = new Image();
+N3.src = "https://yt3.ggpht.com/ytc/AAUvwnjWJo_4aTIX_wYdmKQqm0vWUxKoG68to3JSglKR7w=s68-c-k-c0x00ffffff-no-rj";
+
+var brawStars = new Image();
+brawStars.src = "https://yt3.ggpht.com/ytc/AAUvwnici9k1hHBIAtygBMycjFgIHD-nkyVElfytv88cdg=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var Illunuc = new Image();
+Illinuc.src = "https://yt3.ggpht.com/ytc/AAUvwnh4Rl-DP3UTQ1ErodYKeEPpvlulQe-bYP7QaeZWnQ=s68-c-k-c0x00ffffff-no-rj";
+
+var HAI = new Image();
+HAI.src = "https://yt3.ggpht.com/ytc/AAUvwninYY4ng2Cys4uKTgpK6LGsZ-ifJKzlp0dsoPhr=s68-c-k-c0x00ffffff-no-rj";
+
+var BBG = new Image();
+BBG.src = "https://yt3.ggpht.com/ytc/AAUvwngBFsxEi-hUD4aBucjP9Z_LQrmmC8dlhL_648DoaA=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var Brackeys = new Image();
+Brackeys.src = "https://yt3.ggpht.com/ytc/AAUvwngc0iVBhFEKkdS0rg_1OKI-Q8h3eN0XSzghirJqIw=s68-c-k-c0x00ffffff-no-rj";
+
+var JT = new Image();
+JT.src = "https://yt3.ggpht.com/ytc/AAUvwnihD-8MOK9aXyPStUNPbVn5wWPov4TkwWZqR-w2HA=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var Dani = new Image();
+Dani.src = "https://yt3.ggpht.com/ytc/AAUvwngLQO0aQmi_2CRGTk0sZ-pE7cjYmsBInWEZxonpvg=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var polyMars = new Image();
+polyMars.src = "https://yt3.ggpht.com/ytc/AAUvwngsjSfo63df2rfHnp7rVPnmn3q4X1FNx5lNcOk7-Q=s176-c-k-c0x00ffffff-no-rj-mo";
+
+var charuSharu = new Image();
+charuSharu.src = "https://yt3.ggpht.com/ytc/AAUvwnjd3Wu3tZOzuVlFasWmrI7yRZde9xU8mV5xiYHD=s48-c-k-c0x00ffffff-no-rj-mo";
+
+var drewBinskey = new Image();
+drewBinskey.src = "https://yt3.ggpht.com/ytc/AAUvwnjHYoNnxu-p2WcqRaN-p9eVmZX6OmFL6E_5UTZx=s48-c-k-c0x00ffffff-no-rj-mo";
+
+var Jabrils = new Image();
+Jabrils.src = "https://yt3.ggpht.com/ytc/AAUvwnh05tzUHwVsAn0LBQ74D2SZNfxrFNfCnuHpX6OWPg=s176-c-k-c0x00ffffff-no-rj";
+
+
+
+
+function IMG(width, height, x, y, channel){
+if(channel == codeMyster){
+var cv = document.createElement("canvas");
+cv.class = "cvs";
+const cvs = document.querySelector('cvs');
+cv.style.borderRadius = 50 + "px";
+cv.style.left = x;
+cv.style.top = y;
+const cxt = cvs.getContext("2d");
+cxt.drawImage(codeMyster,0,0,cvs.width,cvs.height);
+
+}
+
+}
